e38f4ef6b0715cb809feab4553ce7c8eda2441b9
Matthew Stevens <mstevens@llst.ca>
2024-01-23 22:11:27 -0800

    Starting to rough in Thought objects

---
+++ Thoughts/thought_object.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var speed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= speed * delta * GlobalData.world_speed
---
+++ global_data.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var world_speed = 10
