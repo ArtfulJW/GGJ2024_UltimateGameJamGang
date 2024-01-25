770d3fc93ce4988b47cb6c1b5b8b462bc90e2ae4
Matthew Stevens <mstevens@llst.ca>
2024-01-25 08:33:14 -0800

    Added a few debug options (esc to quit, R to reset)

--- main.gd
+++ main.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
+@onready var player = $Player
+@onready var start_pos = player.position
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
 
+func _input(event):
+	if event.is_action_pressed("game_reset"):
+		reset()
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+		
+
+func reset():
+	player.x_dest = start_pos.x
+	GlobalData.world_speed = 1
