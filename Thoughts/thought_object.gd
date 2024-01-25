9dd6520ec404706f7ac19a82c8ea77e8fd0cdd86
Matthew Stevens <mstevens@llst.ca>
2024-01-24 19:40:13 -0800

    Player now slides, reacts to clearing or being hit by thoughts

--- Thoughts/thought_object.gd
+++ Thoughts/thought_object.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var sprite_2d = $Sprite2D
 var speed = 80
+var is_collided = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +14,16 @@ func _process(delta):
 	position.x -= speed * delta * GlobalData.world_speed
 	if position.x < -32:
 		GlobalData.world_speed += 0.1
+		if (!is_collided):
+			GlobalData.thought_passed.emit()
 		queue_free()
-
-
 
 
 func _on_body_entered(body):
 	print("collision!")
 	sprite_2d.modulate = Color.AQUA
+	
+	if (!is_collided and body.name == "Player"):
+		body.on_collision()
+		
+	is_collided = true
--- player.gd
+++ player.gd
@@ -1,33 +1,64 @@
 extends CharacterBody2D
 
-
-const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const X_SPEED = 50
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var starting_position = position
+@onready var slide_timer = $SlideTimer
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_sliding = false
+var x_dest = 0.0
+var rotation_dest = 0.0
+var skew_dest = 0.0
+
+
+func _ready():
+	GlobalData.thought_passed.connect(on_thought_passed)
+	x_dest = position.x
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_simulated_floor():
-		velocity.y += gravity * delta
-	else:
-		velocity.y = 0
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_simulated_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	handle_gravity(delta)
+	handle_jump()
+	handle_slide(delta)
+	
+	if(abs(position.x - x_dest) > 0.01):
+		position.x = lerp(position.x,x_dest,delta * X_SPEED)
+	
 	move_and_slide()
 	
 func is_on_simulated_floor():
 	return position.y > starting_position.y
+
+func handle_gravity(delta):
+	if not is_on_simulated_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0
+
+func handle_jump():
+	if Input.is_action_just_pressed("ui_up") and is_on_simulated_floor() and !is_sliding:
+		velocity.y = JUMP_VELOCITY
+
+func handle_slide(delta):
+	if Input.is_action_just_pressed("ui_down") and is_on_simulated_floor() and !is_sliding:
+		rotation_dest = -0.87
+		skew_dest = -0.53
+		is_sliding = true
+		slide_timer.start()
+		
+	if is_sliding and slide_timer.time_left <= 0:
+		rotation_dest = 0.0
+		skew_dest = 0.0
+		is_sliding = false
+
+	if(abs(rotation - rotation_dest) > 0.01):
+		rotation = lerp(rotation, rotation_dest, delta * X_SPEED)
+		skew = lerp(skew, skew_dest, delta * X_SPEED)
+		
+func on_thought_passed():
+	x_dest += 16
+	
+func on_collision():
+	x_dest -= 16
