f326bbc93a870fbfcbbcce5a44d9d2298075c71b
Matthew Stevens <mstevens@llst.ca>
2024-01-27 12:02:39 -0800

    prompts are now modified in inspector data, not hard coded

--- Prompts/prompt.gd
+++ Prompts/prompt.gd
@@ -1,26 +1,48 @@
+
+
 extends Node2D
-var y_mod = 0
-var speed = 12
+
+@export var MAX_X_SPEED: float = 15
+@export var MIN_X_SPEED: float = 10
+@export var MAX_Y_RANGE: float = 10
+@export var MIN_Y_RANGE: float = 5
+@export var MAX_ALPHA: float = 0.5
+@export var MAX_WIGGLE_SALT: float = 10
+@export var MIN_TEXT: int = 20
+@export var MAX_TEXT: int = 50
+@export var label_text = ""
+
 @onready var label = $Label
-var pulse_in = true
 
-# Called when the node enters the scene tree for the first time.
+var pulse_in = randi_range(0,1)
+var y_wiggle = randf_range(0,2*PI)
+var x_speed = randf_range(MIN_X_SPEED,MAX_X_SPEED)
+var y_range = randf_range(MIN_Y_RANGE,MAX_Y_RANGE)
+var wiggle_salt = randf_range(0, MAX_WIGGLE_SALT)
+var text_size = randi_range(MIN_TEXT,MAX_TEXT)
+
 func _ready():
-	pass # Replace with function body.
+	label.label_settings.font_color.a = randf_range(0,MAX_ALPHA)
+	set_text(label_text)
+	set_text_size(text_size)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x -= delta * speed
-	position.y += sin(y_mod) * 7 * delta
-	y_mod += 7 * delta
+	position.x -= delta * x_speed * GlobalData.world_speed
+	position.y += sin(y_wiggle) * y_range * delta
+	y_wiggle += wiggle_salt * delta
 	
-	if pulse_in:
+	if pulse_in == 0:
 		label.label_settings.font_color.a += delta / 1.5
-		if label.label_settings.font_color.a >= 1:
-			pulse_in = false
+		if label.label_settings.font_color.a >= MAX_ALPHA:
+			pulse_in = 1
 	else:
 		label.label_settings.font_color.a -= delta / 1.5
 		if label.label_settings.font_color.a <= 0:
-			pulse_in = true
+			pulse_in = 0
+
+func set_text(text):
+	label_text = text
+	label.text = label_text
 	
+func set_text_size(size):
+	label.label_settings.font_size = size
