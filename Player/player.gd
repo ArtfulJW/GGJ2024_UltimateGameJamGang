7724adee462977d8f0526607161fde0874678cb3
Matthew Stevens <mstevens@llst.ca>
2024-01-27 20:33:21 -0800

    added landing effects to player

--- Player/player.gd
+++ Player/player.gd
@@ -8,11 +8,13 @@ const X_SPEED = 50
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var collision_shape_2d_slide = $CollisionShape2D_slide
 @onready var collision_shape_2d_upright = $CollisionShape2D_upright
+@onready var landing_vfx = $LandingVFX
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_sliding = false
 var x_dest = 0.0
 var is_dead = false
+var just_jumped = false
 
 
 func _ready():
@@ -45,10 +47,14 @@ func handle_gravity(delta):
 		animated_sprite_2d.play("jumping")
 	else:
 		velocity.y = 0
+		if just_jumped:
+			landing_vfx.play("default")
+			just_jumped = false
 
 func handle_jump():
 	if Input.is_action_just_pressed("ui_up") and is_on_simulated_floor() and !is_sliding:
 		velocity.y = JUMP_VELOCITY # * GlobalData.world_speed
+		just_jumped = true
 
 func handle_slide(delta):
 	if Input.is_action_just_pressed("ui_down") and is_on_simulated_floor() and !is_sliding:
