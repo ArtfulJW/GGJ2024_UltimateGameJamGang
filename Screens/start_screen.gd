461ecc46f8efbfb746a8615ca342ecb5fdfcb79c
Jay Wang <jwang550@my.bcit.ca>
2024-01-28 14:11:56 -0800

    Hand over to Matt

--- Screens/start_screen.gd
+++ Screens/start_screen.gd
@@ -2,6 +2,9 @@ extends CanvasLayer
 @onready var reset_timer = $ResetTimer
 @export var main : Node2D
 
+var _IsInstructionsScreenVisible = false
+var _IsCreditScreenVisible = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().paused = true
@@ -15,3 +18,15 @@ func handle_restart():
 	visible = true
 	main.reset()
 
+
+
+func _on_button_2_button_down():
+	_IsInstructionsScreenVisible = true
+	$Control/InstructionScreen.visible = _IsCreditScreenVisible
+	pass # Replace with function body.
+
+
+func _on_button_3_button_down():
+	_IsCreditScreenVisible = true
+	$Control/CreditScreen.visible = _IsCreditScreenVisible
+	pass # Replace with function body.
