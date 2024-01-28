fbe145c30e815ee173f8489a8e0bb283a66cb425
Matthew Stevens <mstevens@llst.ca>
2024-01-27 18:12:21 -0800

    clouds pop

--- Thoughts/MinorThought/thought_object.gd
+++ Thoughts/MinorThought/thought_object.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-var sprite_2d
+@onready var cloud_animation = $CloudAnimation
+@onready var collision_shape_2d = $CollisionShape2D
+
 var speed = 80
 var is_collided = false
 var is_passed = false
@@ -8,13 +10,13 @@ var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var random_value = randi() % 3 + 1
-	if (random_value == 1):
-		sprite_2d=$Cloud1Animation
-	elif random_value == 2:
-		sprite_2d=$Cloud2Animation
+	var random_value = randi_range(0,2)
+	if (random_value == 0):
+		cloud_animation.play("cloud_1")
+	elif random_value == 1:
+		cloud_animation.play("cloud_2")
 	else:
-		sprite_2d=$Cloud3Animation
+		cloud_animation.play("cloud_3")
 
 func set_player(new_player):
 	player = new_player
@@ -35,10 +37,14 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	sprite_2d.modulate = Color.AQUA
-	
 	if (!is_collided and body.name == "Player"):
 		body.on_collision()
-		# GlobalData.world_speed -= GlobalData.world_delta
+		is_collided = true
+		cloud_animation.play("pop")
+		collision_shape_2d.disabled = true
 		
-	is_collided = true
+		
+func _on_cloud_animation_animation_finished():
+	if is_collided:
+		cloud_animation.stop()
+		queue_free()
