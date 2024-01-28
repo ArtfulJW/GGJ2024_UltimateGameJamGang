071b5ee53d2c0e3c2bc5056ce1a1e43b79e64b3a
Matthew Stevens <mstevens@llst.ca>
2024-01-28 14:40:43 -0800

    credits and instructions

--- Screens/start_screen.gd
+++ Screens/start_screen.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 @onready var reset_timer = $ResetTimer
 @onready var intro_beat = $intro_beat
+@onready var credits = $credits
+@onready var instructions = $instructions
+@onready var close_button = $Close_button
+@onready var credit_scroll = $credits/CreditScroll
 
 @export var main : Node2D
 
@@ -10,7 +14,7 @@ func _ready():
 	get_tree().paused = true
 	GlobalData.continue_from_progress.connect(handle_restart)
 
-func _on_button_pressed():
+func _on_start_button_pressed():
 	if intro_beat.visible == false:
 		intro_beat.visible = true
 		return
@@ -22,3 +26,19 @@ func handle_restart():
 	visible = true
 	main.reset()
 
+func _on_credits_button_pressed():
+	credit_scroll.play("credits")
+	credits.visible = true
+	instructions.visible = false 
+	close_button.visible = true
+
+func _on_instructions_button_pressed():
+	credits.visible = false
+	instructions.visible = true
+	close_button.visible = true
+
+
+func _on_close_button_pressed():
+	credits.visible = false
+	instructions.visible = false
+	close_button.visible = false
