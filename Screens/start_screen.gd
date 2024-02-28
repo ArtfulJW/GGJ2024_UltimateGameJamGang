9a17a3f77cf6afe0587d138288155a4e02df4433
LLST-Learning-Games <mstevens@llst.ca>
2024-02-27 19:19:27 -0800

    updated prompts and intro beat text

--- Screens/start_screen.gd
+++ Screens/start_screen.gd
@@ -5,13 +5,21 @@ extends CanvasLayer
 @onready var instructions = $instructions
 @onready var close_button = $Close_button
 @onready var credit_scroll = $credits/CreditScroll
+@onready var intro_beat_animation_scroller = $intro_beat/intro_Label/intro_beat_animation_scroller
 
 @export var main : Node2D
+
+var triggered_animation = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().paused = true
 	GlobalData.continue_from_progress.connect(handle_restart)
+
+func _process(delta):
+	if intro_beat.visible == true and not triggered_animation:
+		intro_beat_animation_scroller.play("scroll_intro_beat")
+		triggered_animation = true
 
 func _on_start_button_pressed():
 	if intro_beat.visible == false:
